907a1efe59dbb9ce81959b09a2422ec2f4fe27ab
Masoud-Karami <massoud.karami@gmail.com>
2025-03-11 15:56:42 -0400

    remove .csvs for a freah ones

--- cuda.py
+++
@@ -1,2 +0,0 @@
-import tensorflow as tf
-print(tf.sysconfig.get_build_info())
--- testgpu.py
+++
@@ -1,7 +0,0 @@
-all_experiments = {'ProbabilisticReasoning', 'HorizonTask', 'RestlessBandit',
-                       'InstrumentalLearning', 'TwoStepTask', 'BART', 'SerialMemoryTask', 'TemporalDiscounting'}
-
-excluded_experiments = {'ProbabilisticReasoning', 'HorizonTask', 'RestlessBandit', 'InstrumentalLearning',
-                            'TwoStepTask', 'BART', 'SerialMemoryTask', 'TemporalDiscounting'}
-
-print(f'focusing folder: {list((all_experiments - excluded_experiments).elements())}')
